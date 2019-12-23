04aca694042d05922ddeff1298bdf53aff5eb099
HerbertVidela <hvidelac@gmail.com>
2019-12-22 20:07:56 -0500

    chore: update dependencies versions

--- src/image/__tests__/upload.test.ts
+++ src/image/__tests__/upload.test.ts
@@ -3,11 +3,12 @@ import { upload } from '../upload';
 
 describe('upload using client_id', () => {
   let client: Client;
+
   beforeEach(() => {
     client = new Client({ client_id: 'f0ea04148a54268' });
   });
 
-  it.only('works with an image', async () => {
+  it('works with an image', async () => {
     const resp = await upload(client, {
       image: `${__dirname}/1x1.png`,
     });
